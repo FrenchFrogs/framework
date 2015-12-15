84a19cf50aaeed3e9e28c5dd8a2b652e012029ec
Julien Houvion <julien.houvion@wengo.com>
2015-12-15 15:27:04 +0100

    add d function

--- src/function.php
+++ src/function.php
@@ -48,6 +48,21 @@ if (!function_exists('dd')) {
 }
 
 
+if (! function_exists('d')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function d()
+    {
+        array_map(function ($x) {
+            (new Illuminate\Support\Debug\Dumper)->dump($x);
+        }, func_get_args());
+    }
+}
+
 /**
  * Return human format octet size (mo, go etc...)
  *
