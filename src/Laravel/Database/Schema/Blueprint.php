305071a18e09cb1f353fff5a3adc49295ff636c3
Julien Houvion <julien.houvion@wengo.com>
2016-06-20 14:56:41 +0200

    Ajout et amlelioration des methode de generation d id dans les schemas

--- src/Laravel/Database/Schema/Blueprint.php
+++ src/Laravel/Database/Schema/Blueprint.php
@@ -1,12 +1,50 @@
 <?php namespace FrenchFrogs\Laravel\Database\Schema;
 
 
+/**
+ * Extension
+ *
+ * Class Blueprint
+ * @package FrenchFrogs\Laravel\Database\Schema
+ */
 class Blueprint extends \Illuminate\Database\Schema\Blueprint
 {
 
-
-    public function binaryUuid($column)
+    /**
+     * Ajoute une column de type binary UUID
+     *
+     * @param string $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function binaryUuid($column = 'id', $primary = true)
     {
-        return $this->addColumn('binaryuuid', $column, ['length' => 16]);
+        $column = $this->addColumn('binaryuuid', $column, ['length' => 16]);
+
+        // gestion de la clé primaire
+        if ($primary) {
+            $column->primary();
+        }
+
+        return $column;
+    }
+
+
+    /**
+     * Ajoute une colonne id de type string
+     *
+     * @param string $column
+     * @param int $size
+     * @return mixed
+     */
+    public function stringId($column = 'id', $size = 32, $primary = true)
+    {
+        $column = $this->string($column, $size);
+
+        // gestion de la clé primaire
+        if ($primary) {
+            $column->primary();
+        }
+
+        return $column;
     }
 }
